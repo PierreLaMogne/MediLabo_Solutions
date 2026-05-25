4b193f69331c9ba14e54bb05e37537565eacffd2
Pierre LaMogne <pierrelamogne@gmail.com>
2026-05-25 18:13:51 +0200

    Configuration de la BDD et implémentation d'une DataSeed

---
+++ MediLabo_Solutions.PatientService/Data/DataSeed.cs
@@ -0,0 +1,23 @@
+﻿using MediLabo_Solutions.PatientService.Domain;
+
+namespace MediLabo_Solutions.PatientService.Data
+{
+    public class DataSeed
+    {
+        public static void Seed(AppDbContext context)
+        {
+            if (context.Patients.Any()) return;
+
+            var patients = new[]
+            {
+                new Patient ("Test", "TestNone", new DateOnly(1966, 12, 31), "F", "01 Brookside St", "100-222-3333"),
+                new Patient ("Test", "TestBorderline", new DateOnly(1945, 6, 24), "M", "2 High St", "200-333-4444"),
+                new Patient ("Test", "TestInDanger", new DateOnly(2004, 6, 18), "M", "3 Club Road", "300-444-5555"),
+                new Patient ("Test", "TestEarlyOnset", new DateOnly(2002, 6, 28), "F", "4 Valley Dr", "400-555-6666")
+            };
+
+            context.Patients.AddRange(patients);
+            context.SaveChanges();
+        }
+    }
+}
--- MediLabo_Solutions.PatientService/Program.cs
+++ MediLabo_Solutions.PatientService/Program.cs
@@ -1,6 +1,36 @@
+using MediLabo_Solutions.PatientService.Data;
+using MediLabo_Solutions.PatientService.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuration de la base de données
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configuration d'Identity
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configuration des controllers
+builder.Services.AddControllers();
+
+// Enregistrement des services
+builder.Services.AddScoped<IPatientService, PatientService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// DataSeed lorsque la DB est vide au lancement
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+    DataSeed.Seed(context);
+}
+
+app.MapControllers();
 
 app.Run();
