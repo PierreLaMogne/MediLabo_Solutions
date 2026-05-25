46316acf4fd6f6cd01b776bfe5ab0b20cc5f1bf0
Pierre LaMogne <pierrelamogne@gmail.com>
2026-05-25 17:45:38 +0200

    Création du DbContext

---
+++ MediLabo_Solutions.PatientService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MediLabo_Solutions.PatientService.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediLabo_Solutions.PatientService.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Patient> Patients { get; set; } = null!;
+    }
+}
