ab91dc9418b5a53bf0e7bf47cee8e448468fe94d
Pierre LaMogne <pierrelamogne@gmail.com>
2026-05-25 17:39:07 +0200

    Création du POCO Patient

---
+++ MediLabo_Solutions.PatientService/Domain/Patient.cs
@@ -0,0 +1,25 @@
+﻿namespace MediLabo_Solutions.PatientService.Domain
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Prénom { get; set; } = string.Empty;
+        public string Nom { get; set; } = string.Empty;
+        public DateOnly DateDeNaissance { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public string? AdressePostale { get; set; }
+        public string? NuméroDeTéléphone { get; set; }
+
+        // Constructeur pour Entity Framework
+        public Patient(string prénom, string nom, DateOnly dateDeNaissance, string genre,
+                        string? adressePostale = null, string? numéroDeTéléphone = null)
+        {
+            Prénom = prénom;
+            Nom = nom;
+            DateDeNaissance = dateDeNaissance;
+            Genre = genre;
+            AdressePostale = adressePostale;
+            NuméroDeTéléphone = numéroDeTéléphone;
+        }
+    }
+}
