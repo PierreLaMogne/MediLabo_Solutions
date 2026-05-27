311810fc471ddabc0cb397e87eec850448f9f395
Pierre LaMogne <pierrelamogne@gmail.com>
2026-05-27 14:45:46 +0200

    Création du projet Shared (+copie de Exceptions et Models)

---
+++ MediLabo_Solutions.Shared/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MediLabo_Solutions.Shared.Models
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
