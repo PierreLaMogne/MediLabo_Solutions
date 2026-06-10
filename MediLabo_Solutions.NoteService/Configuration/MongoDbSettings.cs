52e3bc7d7cb75c287cc38a63639688d4de94a21e
Pierre LaMogne <pierrelamogne@gmail.com>
2026-06-10 09:36:45 +0200

    Configuration de MongoDB

---
+++ MediLabo_Solutions.NoteService/Configuration/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MediLabo_Solutions.NoteService.Configuration
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string NotesCollectionName { get; set; } = string.Empty;
+    }
+}
--- MediLabo_Solutions.NoteService/Program.cs
+++ MediLabo_Solutions.NoteService/Program.cs
@@ -1,6 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using MediLabo_Solutions.NoteService.Configuration;
+using MongoDB.Driver;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuration MongoDB
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
+{
+    var settings = builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>();
+    return new MongoClient(settings!.ConnectionString);
+});
+
+builder.Services.AddScoped(serviceProvider =>
+{
+    var settings = builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>();
+    var client = serviceProvider.GetRequiredService<IMongoClient>();
+    return client.GetDatabase(settings!.DatabaseName);
+});
+
+var app = builder.Build();
 
 app.Run();
