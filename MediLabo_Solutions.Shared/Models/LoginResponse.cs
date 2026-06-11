4587927a700bc341da224e0b4c2096ae9c865835
Pierre LaMogne <pierrelamogne@gmail.com>
2026-06-11 15:30:19 +0200

    Remplacement de LoginResponse par AuthResponse

--- MediLabo_Solutions.Shared/Models/LoginResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MediLabo_Solutions.Shared.Models
-{
-    public class LoginResponse
-    {
-        public string Token { get; set; } = string.Empty;
-        public DateTime Expiration { get; set; }
-    }
-}
