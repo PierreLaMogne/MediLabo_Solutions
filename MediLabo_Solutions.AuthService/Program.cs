5767812b5294039e7476bd469877c500c108f536
Pierre LaMogne <pierrelamogne@gmail.com>
2026-06-11 17:18:06 +0200

    Configuration d'Identity (+DbContext)

--- MediLabo_Solutions.AuthService/Program.cs
+++ MediLabo_Solutions.AuthService/Program.cs
@@ -1,6 +1,33 @@
+using MediLabo_Solutions.AuthService.Data;
 using MediLabo_Solutions.AuthService.Services;
 using MediLabo_Solutions.ExceptionHandler.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuration de la base de données
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AuthDb")));
+
+// Configuration d'Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Configuration des options de verrouillage
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    options.User.RequireUniqueEmail = false;
+})
+.AddEntityFrameworkStores<AuthDbContext>()
+.AddDefaultTokenProviders();
 
 // Enregistrement du service d'authentification
 builder.Services.AddScoped<IAuthAppService, AuthAppService>();
