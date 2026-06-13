3a55cf27cbcc3590d1839872b4f75b32e5c22abe
Pierre LaMogne <pierrelamogne@gmail.com>
2026-06-13 12:13:41 +0200

    Création du modèle DiabeteRiskAssesmentDto

---
+++ MediLabo_Solutions.Shared/Models/DiabetesRiskAssesmentDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediLabo_Solutions.Shared.Models
+{
+    public class DiabetesRiskAssessmentDto
+    {
+        public int PatientId { get; set; }
+        public int Age { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public RiskLevel RiskLevel { get; set; }
+        public int TriggerTermsCount { get; set; }
+        public HashSet<string> IdentifiedTriggers { get; set; } = new();
+        public DateTime AssessmentDate { get; set; } = DateTime.UtcNow;
+    }
+
+    public enum RiskLevel
+    {
+        None,       // Aucun risque
+        Borderline, // Risque limité
+        InDanger,   // Danger
+        EarlyOnset  // Apparition précoce
+    }
+}
