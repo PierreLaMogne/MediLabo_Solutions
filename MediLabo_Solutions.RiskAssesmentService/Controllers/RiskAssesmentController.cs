0a90a884554dce6a3d55461a7df625fab533c4ef
Pierre LaMogne <pierrelamogne@gmail.com>
2026-06-13 16:32:00 +0200

    Ajout du RiskAssesmentController

---
+++ MediLabo_Solutions.RiskAssesmentService/Controllers/RiskAssesmentController.cs
@@ -0,0 +1,24 @@
+﻿using MediLabo_Solutions.RiskAssesmentService.Services;
+using MediLabo_Solutions.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediLabo_Solutions.RiskAssesmentService.Controllers
+{
+    [ApiController]
+    [Route("api/riskassesment")]
+    [Authorize]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public class RiskAssesmentController(IRiskAssesmentAppService riskAssesmentAppService) : Controller
+    {
+        [HttpGet("{patientId}")]
+        [ProducesResponseType(typeof(DiabetesRiskAssessmentDto), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetRiskAssessment(int patientId)
+        {
+            var result = await riskAssesmentAppService.AssessDiabeteRiskAsync(patientId);
+            return Ok(result);
+        }
+    }
+}
